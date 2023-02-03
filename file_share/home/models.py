b86f2f8baef59afdfea85ec626e43554c0e97b03
Prakhyath Bhandary <prakhz16@gmail.com>
2023-02-03 12:42:27 +0530

    initial setup

---
+++ file_share/home/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from uuid import uuid
+import os
+
+# Create your models here.
+
+class Store_Folder(models.Model):
+    uid = models.UUIDField(primary_key=True, editable= False, default=uuid.uuid)
+    created_at = models.DateTimeField(auto_now=True)
+
+
+def get_upload_path(instance , filename):
+    return os.path.join(str(instance.folder.uid) , filename)
+
+class Upload_Files(models.Model):
+    folder = models.ForeignKey(Store_Folder, on_delete=models.CASCADE )
+    file = models.FileField(upload_to= get_upload_path)
+    created_at = models.DateTimeField(auto_now=True)
